83d1cd8964ca5442fe187248c7431ab21fc3b365
Ernandy Lucena <83795827+ErnandyLucena@users.noreply.github.com>
2025-03-18 10:00:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,30 +3,30 @@
         const nextBtn = document.getElementById('nextBtn');
       
         let index = 0;
-        const totalCards = 8; // Número total de cards
-        const cardWidth = 288; // Largura de cada card (w-64 é 16rem, ou 256px, adicionei o margin m-4 que é 1rem)
+        const totalCards = 8; 
+        const cardWidth = 288; 
       
-        // Função para ir para o próximo card
+       
         nextBtn.addEventListener('click', () => {
           if (index < totalCards - 1) {
             index++;
-            carousel.style.transform = `translateX(-${cardWidth * index + 16 * index}px)`; // Considerando o espaçamento entre os cards
+            carousel.style.transform = `translateX(-${cardWidth * index + 16 * index}px)`; 
           }
         });
       
-        // Função para voltar ao card anterior
+     
         prevBtn.addEventListener('click', () => {
           if (index > 0) {
             index--;
-            carousel.style.transform = `translateX(-${cardWidth * index + 16 * index}px)`; // Considerando o espaçamento entre os cards
+            carousel.style.transform = `translateX(-${cardWidth * index + 16 * index}px)`; 
           }
         });
 
         const menuToggle = document.getElementById('menu-toggle');
   const navbar = document.getElementById('navbar-default');
 
-  // Adiciona um evento de clique ao botão de menu
+
   menuToggle.addEventListener('click', () => {
-    // Alterna a visibilidade do menu
+  
     navbar.classList.toggle('hidden');
   });
