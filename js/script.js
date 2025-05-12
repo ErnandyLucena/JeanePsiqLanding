4d3175a0a6798d1e79411ee565e7bef0e6788a80
Ernandy Lucena <83795827+ErnandyLucena@users.noreply.github.com>
2025-05-12 15:17:43 -0300

    Add files via upload
    ajustes formulario, logica de envio

--- js/script.js
+++ js/script.js
@@ -13,3 +13,21 @@ navLinks.forEach(link => {
     navbar.classList.add('hidden');
   });
 });
+
+const form = document.getElementById('contatoForm');
+const popup = document.getElementById('popupSucesso');
+const fecharPopup = document.getElementById('fecharPopup');
+
+// Quando o formulário for enviado
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // Impede o envio do formulário real
+
+  // Exibe o popup de sucesso
+  popup.classList.remove('hidden'); // Remove a classe 'hidden' para mostrar o popup
+});
+
+// Quando o botão de fechar for clicado
+fecharPopup.addEventListener('click', function() {
+  popup.classList.add('hidden'); // Oculta o popup
+  form.reset(); // Limpa o formulário
+});
